fee5d313c5b2c4923e5f3ac61e3cb875b7a08ea3
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-12-14 18:35:30 +0100

    Prettier

--- index.d.ts
+++ index.d.ts
@@ -12,8 +12,9 @@ export interface SubprocessEvents extends EventMap {
 
 export type IO = 'inherit' | 'pipe' | 'overlapped' | 'ignore'
 
-export interface Subprocess<M extends SubprocessEvents = SubprocessEvents>
-  extends EventEmitter<M> {
+export interface Subprocess<
+  M extends SubprocessEvents = SubprocessEvents
+> extends EventEmitter<M> {
   readonly exitCode: number | null
   readonly killed: boolean
   readonly pid: number
