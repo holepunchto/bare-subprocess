96dee86acd80c492928c950875709c7a2612b991
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-06-13 18:41:01 +0200

    Update binary IPC test for latest Bare

--- test/fixtures/ipc-binary.js
+++ test/fixtures/ipc-binary.js
@@ -1,7 +1,4 @@
-const Pipe = require('bare-pipe')
-const env = require('bare-env')
-
-const pipe = new Pipe(Number(env.BARE_CHANNEL_FD), { ipc: true })
+const pipe = Bare.IPC
 
 pipe.on('data', (chunk) => {
   pipe.write(Buffer.concat([Buffer.from('echo:'), chunk]))
